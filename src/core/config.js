346db986adda551d3856ba9f3dbf8511b0dc09b6
Ricardo Benavides <coserricky98@gmail.com>
2021-11-09 14:54:56 -0600

    "Subida 2"

---
+++ src/core/config.js
@@ -0,0 +1,13 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+
+    apiKey: "AIzaSyBXknscZ9q8H332wEk23cHwoALTwqT0it8",
+  authDomain: "photoninnovationhub1.firebaseapp.com",
+  projectId: "photoninnovationhub1",
+  storageBucket: "photoninnovationhub1.appspot.com",
+  messagingSenderId: "839986920010",
+  appId: "1:839986920010:web:20f19e0a6e09d2218cc4b3",
+  measurementId: "G-134ZJQENYW"
+
+
+};
